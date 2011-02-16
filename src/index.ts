66011114cd405d0a2a3a3efae70f6a0cd221e0a4
Shahaed Hasan <shahaed77@gmail.com>
2011-02-16 14:00:00 +0100

    edit a file

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,7 @@
 /**
  * Hypertext Transfer Protocol (HTTP) response status codes.
  * @see {@link https://tools.ietf.org/html/rfc7231#section-6.3.3}
- */
-enum HttpStatusCode {
-  /**
-   The 100 (Continue) status code indicates that the initial part of a
-   request has been received and has not yet been rejected by the
-   server.  The server intends to send a final response after the
-   request has been fully received and acted upon.
 
-   When the request contains an Expect header field that includes a
-   100-continue expectation, the 100 response indicates that the server
-   wishes to receive the request payload body, as described in
-   Section 5.1.1.  The client ought to continue sending the request and
-   discard the 100 response.
-
-   If the request did not contain an Expect header field containing the
-   100-continue expectation, the client can simply discard this interim
-   response.
    */
   CONTINUE = 100,
 
